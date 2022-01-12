50c08012f95483d213d30e59395dd6e31b9cb68f
Danny80 <73097790+Danny-80@users.noreply.github.com>
2022-01-12 18:41:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+
+# Example Input 1
+
+```
+2400
+```
+
+# Example Output 1
+
+```
+Leap year.
+```
+
+# Example Input 2
+
+```
+1989
+```
+
+# Example Output 2
+
+```
+Not leap year.
+```
---
+++ main.py
@@ -0,0 +1,20 @@
+
+year = int(input("Which year do you want to check? "))
+
+
+year = 2020
+
+#  `on every year that is evenly divisible by 4
+#    **except** every year that is evenly divisible by 100
+#    **unless** the year is also evenly divisible by 400`
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print('Leap year')
+    else:
+      print('Not a leap year.')
+  else:
+    print('Leap year')
+else:
+  print('Not a leap year')
